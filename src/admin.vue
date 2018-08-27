5bafca591500ceff562335f307031cf0d27e2f7e
zimogod <18518517713@163.com>
2018-08-27 10:55:09 +0800

    first commit

---
+++ src/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="admin">
+	我是主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'admin'
+}
+</script>
+
+<style>
+#admin {
+  width:100%;
+  height:100%;
+  background:#ccc;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Admin from '../admin';
+Vue.use(Router)
+
+const router = new Router({
+  mode:'hash',
+  routes: [
+    {
+      path: '/',
+      name: 'admin',
+      component: Admin
+    }
+  ]
+})
+export default router;
